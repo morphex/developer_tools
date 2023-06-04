81eae6f4a76c5decda9b2edec7b3737819572675
Morten W. Petersen <morphex@gmail.com>
2023-06-04 18:01:22 +0200

    Adding support for environment variable DEBUG

--- utilities.py
+++ utilities.py
@@ -1,4 +1,9 @@
-DEBUG = False
+import sys, os
+
+if os.environ.get("DEBUG", "") in ("yes", "y", "true", "True"):
+    DEBUG=True
+else:
+    DEBUG = False
 
 def DEBUG_PRINT(*arguments):
     if DEBUG:
